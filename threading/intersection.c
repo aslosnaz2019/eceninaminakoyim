faa1250a9f4a04946ac2a1b4b7d7ef0b1c6a6185
aslosnaz2019 <aslosnaz2019@icloud.com>
2026-03-19 19:36:04 +0100

    Add advanced intersection solution
    r

--- threading/intersection.c
+++ threading/intersection.c
@@ -11,7 +11,7 @@
 #include "intersection_time.h"
 #include "input.h"
 
-/*
+/* 
  * curr_arrivals[][][]
  *
  * A 3D array that stores the arrivals that have occurred
@@ -31,14 +31,16 @@ static Arrival curr_arrivals[4][3][20];
 static sem_t semaphores[4][3];
 
 /*
- * Advanced solution: 4 mutexes, one per quadrant of the intersection.
+ * We divide the intersection into 4 quadrants:
+ *
+ *   0 = NW
+ *   1 = NE
+ *   2 = SE
+ *   3 = SW
+ *
+ * A car must lock all quadrants its path uses.
  */
-static pthread_mutex_t zone_mutexes[4] = {
-  PTHREAD_MUTEX_INITIALIZER,
-  PTHREAD_MUTEX_INITIALIZER,
-  PTHREAD_MUTEX_INITIALIZER,
-  PTHREAD_MUTEX_INITIALIZER
-};
+static pthread_mutex_t quadrant_mutex[4];
 
 /* Thread args for each traffic light */
 typedef struct {
@@ -54,83 +56,166 @@ typedef struct {
  */
 static void* supply_arrivals()
 {
-  int num_curr_arrivals[4][3] = {{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
+  int num_curr_arrivals[4][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
+  // for every arrival in the list
   for (int i = 0; i < (int)(sizeof(input_arrivals) / sizeof(Arrival)); i++)
   {
+    // get the next arrival in the list
     Arrival arrival = input_arrivals[i];
+    // wait until this arrival is supposed to arrive
     sleep_until_arrival(arrival.time);
+    // store the new arrival in curr_arrivals
     curr_arrivals[arrival.side][arrival.direction][num_curr_arrivals[arrival.side][arrival.direction]] = arrival;
     num_curr_arrivals[arrival.side][arrival.direction] += 1;
+    // increment the semaphore for the traffic light that the arrival is for
     sem_post(&semaphores[arrival.side][arrival.direction]);
   }
 
   return 0;
 }
 
-static void get_required_zones(int side, int direction, int zones[2], int *count)
+/*
+ * get_quadrants()
+ *
+ * Returns which quadrants are used by a car entering from (side, direction).
+ * The quadrants are always returned in increasing order.
+ *
+ * Chosen mapping:
+ *
+ * NORTH:
+ *   LEFT      -> 1, 2, 3
+ *   STRAIGHT  -> 1, 2
+ *   RIGHT     -> 1
+ *
+ * EAST:
+ *   LEFT      -> 2, 3, 0
+ *   STRAIGHT  -> 2, 3
+ *   RIGHT     -> 2
+ *
+ * SOUTH:
+ *   LEFT      -> 3, 0, 1
+ *   STRAIGHT  -> 3, 0
+ *   RIGHT     -> 3
+ *
+ * WEST:
+ *   LEFT      -> 0, 1, 2
+ *   STRAIGHT  -> 0, 1
+ *   RIGHT     -> 0
+ */
+static int get_quadrants(int side, int direction, int q[])
 {
-  *count = 0;
-
   if (side == NORTH)
   {
-    if (direction == RIGHT)
-    { zones[0] = 0; *count = 1; }
+    if (direction == LEFT)
+    {
+      q[0] = 1;
+      q[1] = 2;
+      q[2] = 3;
+      return 3;
+    }
     else if (direction == STRAIGHT)
-    { zones[0] = 0; zones[1] = 2; *count = 2; }
-    else 
-    { zones[0] = 1; zones[1] = 3; *count = 2; }
+    {
+      q[0] = 1;
+      q[1] = 2;
+      return 2;
+    }
+    else /* RIGHT */
+    {
+      q[0] = 1;
+      return 1;
+    }
   }
   else if (side == EAST)
   {
-    if (direction == RIGHT)
-    { zones[0] = 1; *count = 1; }
+    if (direction == LEFT)
+    {
+      q[0] = 0;
+      q[1] = 2;
+      q[2] = 3;
+      return 3;
+    }
     else if (direction == STRAIGHT)
-    { zones[0] = 0; zones[1] = 3; *count = 2; }
-    else 
-    { zones[0] = 2; zones[1] = 3; *count = 2; }
+    {
+      q[0] = 2;
+      q[1] = 3;
+      return 2;
+    }
+    else /* RIGHT */
+    {
+      q[0] = 2;
+      return 1;
+    }
   }
   else if (side == SOUTH)
   {
-    if (direction == RIGHT)
-    { zones[0] = 3; *count = 1; }
+    if (direction == LEFT)
+    {
+      q[0] = 0;
+      q[1] = 1;
+      q[2] = 3;
+      return 3;
+    }
     else if (direction == STRAIGHT)
-    { zones[0] = 1; zones[1] = 3; *count = 2; }
-    else
-    { zones[0] = 0; zones[1] = 2; *count = 2; }
+    {
+      q[0] = 0;
+      q[1] = 3;
+      return 2;
+    }
+    else /* RIGHT */
+    {
+      q[0] = 3;
+      return 1;
+    }
   }
-  else
+  else /* WEST */
   {
-    if (direction == RIGHT)
-    { zones[0] = 2; *count = 1; }
+    if (direction == LEFT)
+    {
+      q[0] = 0;
+      q[1] = 1;
+      q[2] = 2;
+      return 3;
+    }
     else if (direction == STRAIGHT)
-    { zones[0] = 1; zones[1] = 2; *count = 2; }
-    else /* LEFT */
-    { zones[0] = 0; zones[1] = 1; *count = 2; }
-  }
-
-  if (*count == 2 && zones[0] > zones[1])
-  {
-    int tmp = zones[0]; zones[0] = zones[1]; zones[1] = tmp;
+    {
+      q[0] = 0;
+      q[1] = 1;
+      return 2;
+    }
+    else /* RIGHT */
+    {
+      q[0] = 0;
+      return 1;
+    }
   }
 }
 
-static void lock_zones(int side, int direction)
+/*
+ * lock_quadrants()
+ *
+ * Lock all required quadrants in increasing order.
+ * This fixed order prevents deadlock.
+ */
+static void lock_quadrants(int q[], int count)
 {
-  int zones[2];
-  int count = 0;
-  get_required_zones(side, direction, zones, &count);
   for (int i = 0; i < count; i++)
-    pthread_mutex_lock(&zone_mutexes[zones[i]]);
+  {
+    pthread_mutex_lock(&quadrant_mutex[q[i]]);
+  }
 }
 
-static void unlock_zones(int side, int direction)
+/*
+ * unlock_quadrants()
+ *
+ * Unlock in reverse order.
+ */
+static void unlock_quadrants(int q[], int count)
 {
-  int zones[2];
-  int count = 0;
-  get_required_zones(side, direction, zones, &count);
   for (int i = count - 1; i >= 0; i--)
-    pthread_mutex_unlock(&zone_mutexes[zones[i]]);
+  {
+    pthread_mutex_unlock(&quadrant_mutex[q[i]]);
+  }
 }
 
 /*
@@ -147,9 +232,16 @@ static void* manage_light(void* arg)
 
   while (true)
   {
+    if (get_time_passed() >= END_TIME)
+    {
+      break;
+    }
+
     int remaining = END_TIME - get_time_passed();
     if (remaining <= 0)
+    {
       break;
+    }
 
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
@@ -160,22 +252,28 @@ static void* manage_light(void* arg)
     if (result == -1)
     {
       if (errno == ETIMEDOUT)
+      {
         break;
-      perror("sem_timedwait");
-      break;
+      }
+      else
+      {
+        perror("sem_timedwait");
+        break;
+      }
     }
 
     if (get_time_passed() >= END_TIME)
+    {
       break;
+    }
 
-    Arrival car = curr_arrivals[side][direction][next_arrival++];
+    Arrival car = curr_arrivals[side][direction][next_arrival];
+    next_arrival++;
 
-    /*
-     * supply_arrivals() already slept until car.time before posting the semaphore,
-     * so the car has already arrived. Do NOT sleep again here.
-     */
+    int q[3];
+    int count = get_quadrants(side, direction, q);
 
-    lock_zones(side, direction);
+    lock_quadrants(q, count);
 
     printf("traffic light %d %d turns green at time %d for car %d\n",
            side, direction, get_time_passed(), car.id);
@@ -187,26 +285,40 @@ static void* manage_light(void* arg)
            side, direction, get_time_passed());
     fflush(stdout);
 
-    unlock_zones(side, direction);
+    unlock_quadrants(q, count);
   }
 
   return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-  (void)argc; (void)argv;
+  (void)argc;
+  (void)argv;
 
   pthread_t supply_thread;
   pthread_t light_threads[4][3];
   LightArgs light_args[4][3];
 
+  // create semaphores to wait/signal for arrivals
   for (int i = 0; i < 4; i++)
+  {
     for (int j = 0; j < 3; j++)
+    {
       sem_init(&semaphores[i][j], 0, 0);
+    }
+  }
 
+  // initialize quadrant mutexes
+  for (int i = 0; i < 4; i++)
+  {
+    pthread_mutex_init(&quadrant_mutex[i], NULL);
+  }
+
+  // start the timer
   start_time();
 
+  // create a thread per traffic light that executes manage_light
   for (int i = 0; i < 4; i++)
   {
     for (int j = 0; j < 3; j++)
@@ -217,20 +329,34 @@ int main(int argc, char *argv[])
     }
   }
 
+  // create a thread that executes supply_arrivals
   pthread_create(&supply_thread, NULL, supply_arrivals, NULL);
 
+  // wait for all threads to finish
   pthread_join(supply_thread, NULL);
 
   for (int i = 0; i < 4; i++)
+  {
     for (int j = 0; j < 3; j++)
+    {
       pthread_join(light_threads[i][j], NULL);
+    }
+  }
 
+  // destroy semaphores
   for (int i = 0; i < 4; i++)
+  {
     for (int j = 0; j < 3; j++)
+    {
       sem_destroy(&semaphores[i][j]);
+    }
+  }
 
+  // destroy mutexes
   for (int i = 0; i < 4; i++)
-    pthread_mutex_destroy(&zone_mutexes[i]);
+  {
+    pthread_mutex_destroy(&quadrant_mutex[i]);
+  }
 
   return 0;
 }
